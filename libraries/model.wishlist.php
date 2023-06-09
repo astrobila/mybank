2dd6006a411f966cd6c24b537f906791b29baa2b
astrobila <astro.bila@gmail.com>
2023-06-10 00:56:41 +0700

    .

--- libraries/model.wishlist.php
+++ libraries/model.wishlist.php
@@ -1,5 +1,36 @@
 <?php
 
 class WishlistModel {
-  
+
+  public static function getAll($user_id, $page = 0, $max_rows = 10) 
+  {
+
+  }
+
+  public static function getWishlistByStatus($user_id, $status) 
+  {
+
+  }
+
+  public static function addProgress() 
+  {
+
+  }
+
+  public static function changeStatus () {
+
+  }
+
+  public static function add () {
+
+  }
+
+  public static function edit () {
+
+  }
+
+  public static function delete () {
+    
+  }
+
 }
