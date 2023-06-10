ff9e03504890eee13ef3d73c4af83a3b12c8f8fa
astrobila <astro.bila@gmail.com>
2023-06-10 10:18:46 +0700

    ignore config

--- libraries/model.transactions.php
+++ libraries/model.transactions.php
@@ -48,10 +48,62 @@ class TransactionsModel {
   ];
   }
 
-  public static function getTransactionByAccount($user_id, $account)
+  public static function getTransactionByAccount($user_id, $page = 0, $max_rows = 10, $account)
   {
-    # code...
+    $db = DB::getInstance();
 
+    $user_id = filter_var($user_id, FILTER_VALIDATE_INT) ? $user_id : 0;
+    $count = $db->query('SELECT COUNT(*) FROM transactions')->fetch_assoc()['COUNT(*)'];
+    
+    $max_page = ceil($count / $max_rows);
+    $max_page = $max_page < 1 ? 1 : $max_page;
+    $page = $page < 1 ? 1 : $page;
+    $page = $page > $max_page ? $max_page : $page;
+    $start_offset = ($page - 1) * $max_rows;
+
+    $account_int = 0;
+
+    switch ($account) {
+      case "Credit" :
+        $account_int = 1;
+      case "Debit" :
+        $account_int = 2;
+      case "Savings" :
+        $account_int = 3;
+    }
+
+    $result = $db->query("
+    SELECT 
+      *
+    FROM 
+      transactions 
+    WHERE
+      user_id = $user_id AND account = $account_int       
+    ORDER BY
+      date_time_created DESC
+    LIMIT 
+      $start_offset, $max_rows
+  ");
+
+  $rows = [];
+
+  if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+      $rows[] = $row;
+    }
+  }
+
+  return [
+    'all_count' => $count,
+    'max_page' => $max_page,
+
+    'count' => $result->num_rows,
+    'page' => $page,
+    
+    'start_offset' => $start_offset,
+
+    'data' => $rows,
+  ];
   }
 
   public static function getBalance($user_id)
