0a639db6895cc5736b50a7bd7d2ccd91384d7af1
Indira <indira6166@gmail.com>
2023-06-17 04:40:48 +0700

    index

--- index.php
+++ index.php
@@ -7,9 +7,49 @@ include 'layouts/inc.header.tag.php';
 include 'layouts/inc.navbar.php';
 include 'layouts/inc.content.begin.php';
 
-Helpers::noLoginRedirect('signin.php');
+// $id = UserSession::get_id();
+// $data = TransactionsModel::getBalance(UserSession::get_id());
+
+
+Helpers::noLoginRedirect('login.php');
 
 ?>
+
+<head>
+<link href="assets\css\index_styling.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="row">
+        <div class="column" style="background-color:#e5e5e5e5;">
+            <div class="index-header">
+                <h2>Welcome, <?=htmlentities(UserSession::get_id())?></h2>
+            </div>
+
+            <div class="balance-box", style="top: 115px;">
+                <div class="balance-text">
+                    <h4>Your Balance</h4>
+                    <h3>RP. <?=htmlentities(TransactionsModel::getBalance(UserSession::get_id()))?></h3>
+                </div>
+            </div>
+            <p></p>
+            <div class="balance-box" style="top: 240px">
+                <div class="wishlist-text">
+                    <h4>Top Wishlist</h4>
+                    <h3>RP. <?=htmlentities(TransactionsModel::getBalance(UserSession::get_id()))?></h3>
+                </div>
+            </div>
+            
+        </div>
+        <div class="column" style="background-color:#e5e5e5e5;">
+            <h2>Column 2</h2>
+            <p>Some text..</p>
+        </div>
+    </div>
+</body>
+    
+
+
+
 
 <?php
 
